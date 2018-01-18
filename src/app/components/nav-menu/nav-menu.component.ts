5eb1ac0298ad523d7e85af7c3ced33f9dc0c3eaf
Methodician <jacobgjohnston@gmail.com>
2018-01-18 09:07:38 -0800

    Final touches. Not adding bootstrap yet...

--- src/app/components/nav-menu/nav-menu.component.ts
+++ src/app/components/nav-menu/nav-menu.component.ts
@@ -1,15 +1,42 @@
 import { Component, OnInit } from '@angular/core';
+import { allNavAnimations } from '@animations/nav.animations'
+import { MediaQueryService } from '@services/media-query.service';
 
 @Component({
   selector: 'fly-nav-menu',
   templateUrl: './nav-menu.component.html',
-  styleUrls: ['./nav-menu.component.scss']
+  styleUrls: ['./nav-menu.component.scss'],
+  animations: [allNavAnimations]
 })
 export class NavMenuComponent implements OnInit {
 
-  constructor() { }
+  public isCollapsed: boolean = true;
+  private navList = [
+    { link: 'home', text: 'HOME' },
+    { link: 'apps', text: 'EXAMPLES' },
+    { link: 'about', text: 'TEAM' },
+    { link: 'contact', text: 'CONTACT' }
+  ];
+
+  constructor(private querySvc: MediaQueryService) { }
 
   ngOnInit() {
   }
+
+  showArrowButton() {
+    const queryGroup = this.querySvc.deviceGroup.value; //  Alternative to subscribing since I'll be calling this function every resize anyway
+    return !(queryGroup == 'desktop' || queryGroup == 'iPadLandscape' || queryGroup == 'iPadPortrait');
+  }
+
+  clickedOutside() {
+    console.log('clicked');
+  }
+
+
+  /*showNavBar() {
+      if (this.qrySvc.desktop || this.qrySvc.iPadLandscape || this.qrySvc.iPadPortrait)
+          return true;
+      else return false;
+  }*/
 
 }
