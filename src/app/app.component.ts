34f394cb9ce538b1a2afb19bec957a10bc8800fa
Angular CLI <angular-cli@angular.io>
2018-01-17 16:53:14 -0800

    chore: initial commit from @angular/cli

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'fly-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'fly';
+}
