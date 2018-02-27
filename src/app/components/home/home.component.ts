9838bbf25ed9e6f9bd9e12db5e292fb7ca3c6cbb
Kimberly Bordon <kbordon@gmail.com>
2018-02-26 20:05:57 -0800

    Start layout for projects carousel

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -16,6 +16,8 @@ import { NguCarousel, NguCarouselStore } from '@ngu/carousel';
 })
 export class HomeComponent implements OnInit {
   public carouselBanner: NguCarousel;
+  public carouselBanner2: NguCarousel;
+
   currentDevice: DeviceGroups = DeviceGroups.desktop;
   constructor(
     private mediaSvc: MediaQueryService
@@ -73,7 +75,49 @@ export class HomeComponent implements OnInit {
       loop: false,
       touch: true
     }
+
+    this.carouselBanner2 = {
+      grid: {xs: 1, sm: 1, md: 1, lg: 1, all: 0},
+      slide: 1,
+      speed: 400,
+      interval: 4000,
+      point: {
+        visible: false,
+        pointStyles: `
+          .ngucarouselPoint {
+            list-style-type: none;
+            text-align: center;
+            padding: 12px;
+            margin: 0;
+            white-space: nowrap;
+            overflow: auto;
+            position: absolute;
+            width: 100%;
+            bottom: 40px;
+            left: 0;
+            box-sizing: border-box;
+          }
+          .ngucarouselPoint li {
+            display: inline-block;
+            border-radius: 999px;
+            border: 1px solid rgba(0, 0, 0, 1);
+            background: rgba(0, 0, 0, 0);
+            padding: 5px;
+            margin: 0 3px;
+            transition: .4s ease all;
+          }
+          .ngucarouselPoint li.active {
+              background: black;
+              width: 10px;
+          }
+        `
+      },
+      load: 2,
+      loop: false,
+      touch: true
+    }
   }
+
  
   /* It will be triggered on every slide*/
   onmoveFn(data: NguCarouselStore) {
