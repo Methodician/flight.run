bf9c9303bc89cb2ccbf25bbb18c70b9c99c12edc
Kimberly Bordon <kbordon@gmail.com>
2018-04-25 12:49:17 -0700

    Create survey form component

---
+++ src/app/components/survey/survey-form/survey-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'fly-survey-form',
+  templateUrl: './survey-form.component.html',
+  styleUrls: ['./survey-form.component.scss']
+})
+export class SurveyFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
