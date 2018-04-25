b0be2eaa590febbca85ebf5b218d045461d5b367
Kimberly Bordon <kbordon@gmail.com>
2018-04-25 12:49:03 -0700

    Create survey component

---
+++ src/app/components/survey/survey/survey.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'fly-survey',
+  templateUrl: './survey.component.html',
+  styleUrls: ['./survey.component.scss']
+})
+export class SurveyComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
