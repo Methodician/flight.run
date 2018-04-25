d00834226aa85df5c9f980bf7de927c0ee17a184
Kimberly Bordon <kbordon@gmail.com>
2018-04-25 15:26:42 -0700

    Create method to add to survey

--- src/app/services/survey.service.ts
+++ src/app/services/survey.service.ts
@@ -1,8 +1,18 @@
 import { Injectable } from '@angular/core';
+import { AngularFirestore} from 'angularfire2/firestore';
+import * as firebase from 'firebase';
+
 
 @Injectable()
 export class SurveyService {
 
-  constructor() { }
+  constructor(
+    private db: AngularFirestore
+  ) { }
 
+  submitSurvey(form: any) {
+    form.timestamp = firebase.firestore.FieldValue.serverTimestamp();
+    console.log("hey totally got accessed ", form);
+    return this.db.collection('internshipSurveys').add(form);
+  }
 }
