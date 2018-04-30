0c18e1cd22fe40f815869cc1113ac2df35374f8f
Kimberly Bordon <kbordon@gmail.com>
2018-04-30 08:34:18 -0700

    Fix copy typos

--- src/app/components/survey/survey-form/questions.ts
+++ src/app/components/survey/survey-form/questions.ts
@@ -1,6 +1,6 @@
 export const questions = {
   fr: [
-    "What aspects of web application development and design are you most interested in pursuing and learning more about?",
+    "What aspects of web application development, and design are you most interested in pursuing and learning more about?",
     "What part of web application development and design has been the most challenging to you?",
     "What do you think are your strongest points in web app development?",
     "What do you want to work on with us and what do you hope to get out of this internship?"
@@ -8,12 +8,11 @@ export const questions = {
   aip: [
     "Interactions with stakeholders (talking with people like customers, supervisors, planners, and co-workers and understanding/interpreting their needs)",
     "Staying organized and keeping track of lots of different requirements and tasks in an orderly manner",															
-    "Working with project management and organizational tools (like asana, trello, calendar tools, meistertask, time trackers,  etc…)",				
-    "Working with GIT or other source control tools",																	
-    "Understanding and clearly defining requirements for a project (and helping team members understand)",																	
+    "Working with project management, and organizational tools (like asana, trello, calendar tools, meistertask, time trackers, etc…)",	"Working with GIT or other source control tools",																	
+    "Understanding, and clearly defining requirements for a project (and helping team members understand)",
     "Envisioning user experience and the UX workflow",																	
     "Creating visually aesthetic designs given the constraints of project requirements",																	
-    "Envisioning and designing user interface (what kind of buttons, inputs, error messages, etc should go here and there? Should this form be on one page or split into sections? Is it clear what a user needs to do without detailed instructions?)",															
+    "Envisioning, and designing user interface (what kind of buttons, inputs, error messages, etc should go here and there? Should this form be on one page or split into sections? Is it clear what a user needs to do without detailed instructions?)",
     "Solving logical problems with functions and code",																
     "Solving mathematical problems with functions and code",													
     "Translating visual design specs into code with HTML/CSS",																	
