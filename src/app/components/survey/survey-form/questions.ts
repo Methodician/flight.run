ab526ebfa609825073f35c1a212e57ab3568ae08
Methodician <jacobgjohnston@gmail.com>
2018-05-15 13:12:08 -0700

    Changed questions and wording in form.

--- src/app/components/survey/survey-form/questions.ts
+++ src/app/components/survey/survey-form/questions.ts
@@ -1,29 +1,33 @@
 export const questions = {
   cohorts: [
-    "Epicodus",
+    "May 2018 .NET Portland",
+    "May 2018 Rails Portland",
+    "Independent Contact",
     "Other"
   ],
   fr: [
-    "What aspects of web application development, and design are you most interested in pursuing and learning more about?",
-    "What part of web application development and design has been the most challenging to you?",
-    "What do you think are your strongest points in web app development?",
+    "Just for the record, let me know how long this survey took when you're done. Was it too long? quick and easy? Maybe check the time right now and jot it down here.",
+    "What aspects of app/web development and design are you most interested in pursuing and learning more about?",
+    "What are your least favorite aspects of app/web development and design?",
+    "What would you say are your greatest strengths in the context of development and design?",
+    "What part of app/web development and design has been the most challenging to you?",
     "What do you want to work on with us and what do you hope to get out of this internship?"
   ],
   aip: [
     "Interactions with stakeholders (talking with people like customers, supervisors, planners, and co-workers and understanding/interpreting their needs)",
-    "Staying organized and keeping track of lots of different requirements and tasks in an orderly manner",															
-    "Working with project management, and organizational tools (like asana, trello, calendar tools, meistertask, time trackers, etc…)",	"Working with GIT or other source control tools",																	
+    "Staying organized and keeping track of lots of different requirements and tasks in an orderly manner",
     "Understanding, and clearly defining requirements for a project (and helping team members understand)",
-    "Envisioning user experience and the UX workflow",																	
-    "Creating visually aesthetic designs given the constraints of project requirements",																	
+    "Working with project management tools and organizational tools (like trello, calendar tools, Jira, time trackers, etc…)", "Working with GIT or other source control tools",
+    "Envisioning user experience and the UX workflow",
+    "Creating visually aesthetic designs given the constraints of project requirements",
     "Envisioning, and designing user interface (what kind of buttons, inputs, error messages, etc should go here and there? Should this form be on one page or split into sections? Is it clear what a user needs to do without detailed instructions?)",
-    "Solving logical problems with functions and code",																
-    "Solving mathematical problems with functions and code",													
-    "Translating visual design specs into code with HTML/CSS",																	
-    "Creating animations and transitions with HTML/CSS, JavaScript, or tools like Angular Animations",																
-    "Understanding and designing data structures for storage and retrieval of the data that ultimately drives a web application",	
-    "Writing raw SQL",																
-    "Working with relational data structures (like tables and spreadsheets in rows/columns/cells)",																	
-    "Working with tree-like data structures (like JSON and NoSQL solutions, Firebase, MongoDB, etc...)"	
+    "Solving logical problems with functions and code",
+    "Solving mathematical problems with functions and code",
+    "Translating visual design specs into code with HTML/CSS",
+    "Creating animations and transitions with HTML/CSS, JavaScript, or tools like Angular Animations",
+    "Understanding and designing data structures for storage and retrieval of the data that ultimately drives a web application",
+    "Writing raw SQL",
+    "Working with relational data structures (like tables and spreadsheets in rows/columns/cells)",
+    "Working with tree-like data structures (like JSON and NoSQL solutions, Firebase, MongoDB, etc...)"
   ]
 };
