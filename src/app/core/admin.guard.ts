f02619bda5e597af52404f3214b5bb688ec87219
Ian Goodrich <iangoodrich@hotmail.com>
2018-06-05 16:47:26 -0700

    reverted to default state line. Tutorial version left commented out

--- src/app/core/admin.guard.ts
+++ src/app/core/admin.guard.ts
@@ -11,9 +11,9 @@ export class AdminGuard implements CanActivate {
 
   canActivate(
     next: ActivatedRouteSnapshot,
-      // state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
+      state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
       // return true;
-    state: RouterStateSnapshot): Observable<boolean> {
+    // state: RouterStateSnapshot): Observable<boolean> {
     return this.auth.user$.pipe(
       take(1),
       map(user => user && user.roles.admin ? true : false),
