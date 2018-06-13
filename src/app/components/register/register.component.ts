d85c4af7880238f13e3b5c57d294c73a3d4246dd
Ian Goodrich <iangoodrich@hotmail.com>
2018-06-13 12:23:33 -0700

    update register component

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'fly-register-component',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent implements OnInit {
 
-  constructor( private authservice: AuthService) { }
+  constructor( private authservice: AuthService, private router: Router) { }
 
   ngOnInit() {
   }
@@ -17,6 +18,9 @@ export class RegisterComponent implements OnInit {
   onSignUp(form: NgForm) {
     const email = form.value.email;
     const password = form.value.password;
-    this.authservice.emailSignUp(email, password);
+    const username = form.value.username;
+    const img = form.value.img;
+    this.authservice.emailSignUp(email, password, username, img);
+    this.router.navigate(['']);
   }
 }
