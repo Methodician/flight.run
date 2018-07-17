c33a7588070a3083943531588cfe93496d64053d
Vinayaka Patrick Thompson <thompvin@oregonstate.edu>
2018-07-17 14:39:28 -0700

    Fixed bug caused by adding new component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -43,8 +43,7 @@ import { AuthService } from './services/auth.service';
 import { NotifyService } from './services/notify.service';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
-import { FollowUpSurveyComponent } from './follow-up-survey/follow-up-survey.component';
-import { FollowUpSurveyMainComponent } from './follow-up-survey-main/follow-up-survey-main.component';
+import { FollowUpSurveyMainComponent } from './components/follow-up-survey/follow-up-survey-main/follow-up-survey-main.component';
 
 
 
@@ -65,7 +64,6 @@ import { FollowUpSurveyMainComponent } from './follow-up-survey-main/follow-up-s
     SurveyDetailComponent,
     LoginComponent,
     RegisterComponent,
-    FollowUpSurveyComponent,
     FollowUpSurveyMainComponent
   ],
   imports: [
