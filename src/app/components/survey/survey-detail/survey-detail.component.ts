a72d17817d84b0d1fed953bb6d6cb7b8975dbb91
Vinayaka Patrick Thompson <thompvin@oregonstate.edu>
2018-07-17 10:37:51 -0700

    Updated refrence to questions in survey-detail to reflect moved question

--- src/app/components/survey/survey-detail/survey-detail.component.ts
+++ src/app/components/survey/survey-detail/survey-detail.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { SurveyService } from '@services/survey.service';
 import { ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
-import { questions } from '../../../shared/questions';
+import { questions } from './../questions';
 import { AuthService } from '../../../services/auth.service';
 
 @Component({
