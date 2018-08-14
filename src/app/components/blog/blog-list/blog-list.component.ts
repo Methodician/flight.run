95b88e2c9502a264688f4b262a48da876bd4799b
Kristen <peanutster@gmail.com>
2018-08-14 11:26:47 -0700

    add catergory selector to filter posts

--- src/app/components/blog/blog-list/blog-list.component.ts
+++ src/app/components/blog/blog-list/blog-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { BlogService } from '@services/blog.service';
+import { Router, ActivatedRoute} from '@angular/router';
 
 @Component({
   selector: 'fly-blog-list',
@@ -7,29 +8,48 @@ import { BlogService } from '@services/blog.service';
   styleUrls: ['./blog-list.component.scss']
 })
 export class BlogListComponent implements OnInit {
+  path;
   posts;
   postsMetaData;
-  constructor(private blogService: BlogService) { }
+  categories;
+  categoriesMetaData;
+  constructor(private blogService: BlogService, private router: Router, private route: ActivatedRoute) { }
 
   ngOnInit() {
-    this.getPosts();
+	this.path = this.route.params['_value']['slug'];
+    
+	if(this.path) {
+  	this.getPostsByCategory(this.path);
+	} else {
+  	this.getPosts();
+	}
+	this.getCategories();
   }
 
   async getPosts() {
-    const results = await this.blogService.getPosts();
-    this.posts = results.data;
-    this.postsMetaData = results.meta;
-    console.log(this.posts);
+	const results = await this.blogService.getPosts();
+	this.posts = results.data;
+	this.postsMetaData = results.meta;
   }
 
   async getCategories() {
-    const categories = await this.blogService.getCategories();
-    console.log(categories);
+	const results = await this.blogService.getCategories();
+	this.categories = results.data;
+	this.categoriesMetaData = results.meta;
   }
 
   async getPostsByCategory(slug) {
-    const posts = await this.blogService.getPostsByCategory(slug);
-    console.log(posts);
+	const results = await this.blogService.getPostsByCategory(slug);
+	this.posts = results.recent_posts;
+	this.postsMetaData = results.meta;
+  }
+
+  onChange(slug) {
+	this.router.navigate(['blog/category', slug]);
+	if(this.path) {
+  	this.getPostsByCategory(slug);
+	}
   }
 
 }
+
