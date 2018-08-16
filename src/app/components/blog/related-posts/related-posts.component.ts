56ea8ea4958f45d9da02a0dba29444e0e2c8ad98
Kristen <peanutster@gmail.com>
2018-08-16 11:24:55 -0700

    fix mistake in related posts

--- src/app/components/blog/related-posts/related-posts.component.ts
+++ src/app/components/blog/related-posts/related-posts.component.ts
@@ -8,19 +8,25 @@ import { Router } from '@angular/router';
   styleUrls: ['./related-posts.component.scss']
 })
 export class RelatedPostsComponent implements OnInit {
-  @Input() categories;
+  @Input() post;
   @Output() changePost= new EventEmitter();
   relatedPosts = [];
 
   constructor(private blogService: BlogService, private router: Router) { }
 
   ngOnInit() {
-    this.getRealatedPosts(this.categories[0].slug);
+    
+    this.getRealatedPosts(this.post.categories[0].slug);
   }
 
   async getRealatedPosts(slug) {
     const results = await this.blogService.getPostsByCategory(slug);
-    let allPosts = results.recent_posts;
+    let allPosts = [];
+    results.recent_posts.forEach(relatedPost => {
+      if(relatedPost.slug !== this.post.slug){
+        allPosts.push(relatedPost);
+      }
+    });
     let maxPosts=3;
     if(allPosts.length<3){
       maxPosts=allPosts.length;
