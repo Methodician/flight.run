11aa388b8d1dc033c64c15fd581b5c01996bd9b6
Mike Chu <mcihkue@gmail.com>
2018-08-22 15:58:10 -0700

    Remove view-specific navbar and functions for simulating CSS media query behavior.

--- src/app/components/nav-menu/nav-menu.component.ts
+++ src/app/components/nav-menu/nav-menu.component.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit, OnChanges } from '@angular/core';
-import { allNavAnimations } from '@animations/nav.animations';
-import { MediaQueryService } from '@services/media-query.service';
 import { Router, RouterModule } from '@angular/router';
 import * as smoothscroll from 'smoothscroll-polyfill';
 import { AuthService } from '@services/auth.service';
@@ -8,54 +6,26 @@ import { AuthService } from '@services/auth.service';
 @Component({
   selector: 'fly-nav-menu',
   templateUrl: './nav-menu.component.html',
-  styleUrls: ['./nav-menu.component.scss'],
-  animations: [allNavAnimations]
+  styleUrls: ['./nav-menu.component.scss']
 })
 export class NavMenuComponent implements OnInit {
   // public user: boolean = (this.authSvc.loggedIn);
-  public deviceGroup: string;
-  public isCollapsed = true;
 
   linkList = [
-    // { link: '', text: this.deviceGroup },
-    // { link: 'home', text: 'Home' },
-    { link: 'examples', text: 'Examples' },
-    { link: 'blog', text: 'Blog' }
-  ];
-
-  scrollList = [
-    // { link: 'about', text: 'ABOUT US' },
-    // { link: 'work', text: 'Our Work' },
-    // { link: 'work', text: this.showArrowButton() ? 'Work' : 'Our Work' },
-    // { link: 'partners', text: 'Testimonials' },
-    // { link: 'team', text: 'Our Team' },
-    { link: 'contact', text: 'Contact' },
+    { link: 'examples', text: 'Examples', scroll: false },
+    { link: 'blog', text: 'Blog', scroll: false },
+    { link: 'home', text: 'Contact', scroll: true, scrollAnchor: 'contact' },
   ];
 
   constructor(
     private authSvc: AuthService,
-    private querySvc: MediaQueryService,
     private router: Router
   ) {
     smoothscroll.polyfill();
   }
 
   ngOnInit() {
-    this.querySvc.deviceGroup.subscribe(group => {
-      console.log(group);
-      this.deviceGroup = group;
-      // console.log(' this user in nav' + this.user);
-
-    });
-  }
-
-
-  showArrowButton() {
-    return !(this.deviceGroup === 'desktop' || this.deviceGroup === 'iPadLandscape' || this.deviceGroup === 'iPadPortrait');
-  }
-
-  showNavBar() {
-    return (this.deviceGroup === 'desktop' || this.deviceGroup === 'iPadLandscape' || this.deviceGroup === 'iPadPortrait');
+    // console.log(' this user in nav' + this.user);
   }
 
   scrollTo(selector: string) {
@@ -70,15 +40,10 @@ export class NavMenuComponent implements OnInit {
       const element = document.getElementById(selector);
       element.scrollIntoView({ behavior: 'smooth' });
     }
-
   }
 
   logOut() {
-    console.log('logout clicked'); // REMOVE
     this.authSvc.signOut();
   }
-
-
-
 
 }
