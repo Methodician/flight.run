3e306e65a6309cf5be6bab93a3f83777877fb870
Kristen <peanutster@gmail.com>
2018-08-22 11:44:03 -0700

    add page as Input value

--- src/app/components/case-studies/case-slide/case-slide.component.ts
+++ src/app/components/case-studies/case-slide/case-slide.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit} from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'fly-case-slide',
@@ -6,6 +6,7 @@ import { Component, OnInit} from '@angular/core';
   styleUrls: ['./case-slide.component.scss']
 })
 export class CaseSlideComponent implements OnInit {
+  @Input() page;
 
   constructor() { }
 
