17176af257c6da2e206eae34bb49f3556c3f5de9
Kristen <peanutster@gmail.com>
2018-08-24 14:17:35 -0700

    create functions for add/delete blog slugs

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -22,9 +22,39 @@ export const addMessage = functions.https.onRequest(async (req, res) => {
 });
 
 export const addBlogSlug = functions.https.onRequest(async (req, res) => {
-    console.log(req);
-    const body = req.body;
-    console.log(body);
-    const snapshot = await admin.database().ref('/test-blog-thing').push(body);
+    const slug = req.body.data.id;
+    const timeStamp = new Date();
+    let json = {};
+    json[slug] = timeStamp;
+    const snapshot = await admin.database().ref('/blog-slugs').push(json);
     return;
-})
+});
+
+export const addPageSlug = functions.https.onRequest(async (req, res) => {
+    const slug = req.body.data.id;
+    const type = req.body.data.page_type;
+    const timeStamp = new Date();
+    let json = {};
+    json[slug] = timeStamp;
+    if(type === 'client_case_study'){
+      const snapshot = await admin.database().ref('/case-study-slugs').push(json);
+    }
+
+    return;
+});
+
+export const deleteBlogSlug = functions.https.onRequest(async (req, res) => {
+    const slug = req.body.data.id;
+    const snapshot = await admin.database().ref('/blog-slugs/' +slug).remove();
+    return;
+});
+
+export const addPageSlug = functions.https.onRequest(async (req, res) => {
+  const slug = req.body.data.id;
+  const type = req.body.data.page_type;
+  if(type === 'client_case_study'){
+    const snapshot = await admin.database().ref('/case-study-slugs/' +slug).remove();
+  }
+
+    return;
+});
