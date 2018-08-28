63ccbfbfa1ab16fb3060274c0095791c1e424698
Mike Chu <mcihkue@gmail.com>
2018-08-27 17:11:25 -0700

    Redesign structure, functions, and styles for CaseDetail component.

--- src/app/components/case-studies/case-detail/case-detail.component.ts
+++ src/app/components/case-studies/case-detail/case-detail.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { CaseService } from '@services/case.service';
-import { Router, ActivatedRoute, NavigationEnd } from '@angular/router';
-import { MediaQueryService } from '@services/media-query.service';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'fly-case-detail',
@@ -11,23 +10,12 @@ import { MediaQueryService } from '@services/media-query.service';
 export class CaseDetailComponent implements OnInit {
   slug: string;
   page;
-  showMoreUnstacked = false;
-  showMoreChallenge = false;
-  showMoreAccepted = false;
-  slideTime: number = 10000;
-  deviceGroup;
-  constructor(private caseService: CaseService, private queryService: MediaQueryService, public route: ActivatedRoute, private router: Router) { }
+  toggleOnAll: boolean = false;
+  toggleOnGroup1: boolean = false;
+  toggleOnGroup2: boolean = false;
+  constructor(private caseService: CaseService, public route: ActivatedRoute, private router: Router) { }
 
   ngOnInit() {
-    this.queryService.deviceGroup.subscribe(group => {
-      this.deviceGroup = group;
-    });
-    this.router.events.subscribe((evt) => {
-      if (!(evt instanceof NavigationEnd)) {
-          return;
-      }
-      window.scrollTo(0, 0)
-  });
     this.slug = this.route.params['_value']['slug'];
     this.getPageBySlug(this.slug);
   }
@@ -37,30 +25,26 @@ export class CaseDetailComponent implements OnInit {
     this.page = result.data;
   }
 
-  toggleShowMoreUnstacked(){
-    this.showMoreUnstacked= !this.showMoreUnstacked;
-    if(this.showMoreUnstacked === false){
-      window.scrollTo(0, 350);
+  onToggleAll() {
+    this.toggleOnAll = !this.toggleOnAll;
+  }
+
+  onToggleGroup1() {
+    this.toggleOnGroup1 = !this.toggleOnGroup1;
+  }
+
+  onToggleGroup2() {
+    this.toggleOnGroup2 = !this.toggleOnGroup2;
+  }
+
+  checkWindowWidth() {
+    if (this.toggleOnAll) {
+      this.toggleOnAll = (window.innerWidth < 1024) ? false : this.toggleOnAll;
+    }
+    if (this.toggleOnGroup1 || this.toggleOnGroup2) {
+      this.toggleOnGroup1 = (window.innerWidth >= 1024) ? false : this.toggleOnGroup1;
+      this.toggleOnGroup2 = (window.innerWidth >= 1024) ? false : this.toggleOnGroup2;
     }
   }
 
-  toggleShowMoreChallenge(){
-    this.showMoreChallenge= !this.showMoreChallenge;
-    if(this.showMoreChallenge === false){
-      window.scrollTo(0, 350);
-    }
-  }
-
-  toggleShowMoreAccepted(){
-    this.showMoreAccepted= !this.showMoreAccepted;
-    if(this.showMoreAccepted === false && this.showMoreChallenge === false){
-      window.scrollTo(0, 650);
-    } else if(this.showMoreAccepted === false ){
-      window.scrollTo(0, 1200);
-    }
-  }
-
-  stackContent() {
-    return (this.deviceGroup === 'desktop' || this.deviceGroup === 'iPadLandscape' || this.deviceGroup === 'iPadPortrait');
-  }
 }
--- src/app/components/case-studies/case-preview/case-preview.component.ts
+++ src/app/components/case-studies/case-preview/case-preview.component.ts
@@ -1,17 +1,12 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'fly-case-preview',
   templateUrl: './case-preview.component.html',
   styleUrls: ['./case-preview.component.scss']
 })
-export class CasePreviewComponent implements OnInit {
-
+export class CasePreviewComponent {
   @Input() page;
   constructor() {}
-
-   ngOnInit() { 
-     
-  }
 
 }
