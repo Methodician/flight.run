bacc99cfe8c90fe0b9d355908eb005eca43bfb59
Mike Chu <mcihkue@gmail.com>
2018-08-27 23:07:22 -0700

    Update styles for SupportingImage carousel item component.

--- src/app/components/carousel/carousel-item-supporting-image/carousel-item-supporting-image.component.ts
+++ src/app/components/carousel/carousel-item-supporting-image/carousel-item-supporting-image.component.ts
@@ -1,18 +1,13 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'fly-carousel-item-supporting-image',
   templateUrl: './carousel-item-supporting-image.component.html',
-  styleUrls: ['../carousel-shared/carousel-item.component.scss']
+  styleUrls: ['./carousel-item-supporting-image.component.scss']
 })
-export class CarouselItemSupportingImageComponent implements OnInit {
-
-  @Input() image;
-
+export class CarouselItemSupportingImageComponent {
+  @Input() imageUrl: string;
+  @Input() imageCaption: string;
   constructor() { }
-
-  ngOnInit() {
-    console.log(this.image);
-  }
 
 }
