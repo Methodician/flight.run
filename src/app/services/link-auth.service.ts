61b5819373c9ef0bf5feacf5ca6fd55a46399404
Kristen <peanutster@gmail.com>
2018-09-04 16:21:48 -0700

    change a variable from const to let in confirmSignIn function

--- src/app/services/link-auth.service.ts
+++ src/app/services/link-auth.service.ts
@@ -46,7 +46,7 @@ export class LinkAuthService {
       // the sign-in operation.
       // Get the email if available. This should be available if the user completes
       // the flow on the same device where they started it.
-      const email = window.localStorage.getItem('emailForSignIn');
+      let email = window.localStorage.getItem('emailForSignIn');
       console.log(email);
       if (!email) {
         // User opened the link on a different device. To prevent session fixation
