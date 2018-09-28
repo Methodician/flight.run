acd33d62a6b419620967817b6b12492cfae322fd
Mike Chu <mcihkue@gmail.com>
2018-09-28 13:59:51 -0700

    Add basic structure and logic to select/display images in ImageGallery.

--- src/app/components/image-gallery/image-gallery.component.ts
+++ src/app/components/image-gallery/image-gallery.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'fly-image-gallery',
   templateUrl: './image-gallery.component.html',
   styleUrls: ['./image-gallery.component.scss']
 })
-export class ImageGalleryComponent implements OnInit {
-
+export class ImageGalleryComponent {
+  @Input() images;
+  selectedImageId = 0;
   constructor() { }
 
-  ngOnInit() {
+  selectImage(index) {
+    this.selectedImageId = index;
+  }
+
+  isSelected(index) {
+    return (index === this.selectedImageId) ? true : false;
   }
 
 }
