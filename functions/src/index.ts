aec03cd2dc66517a5b4fb903b6b17e049206a399
Kristen <peanutster@gmail.com>
2018-10-18 09:34:46 -0700

    refactor two functions into one function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -9,45 +9,25 @@ const butter = Butter('2c11ed1f264363ff0e0b87e0e7f30058a444fac8');
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 
-export const handleBlogWebhook = functions.https.onRequest(async (req, res) => {
-  const hookType = req.body.webhook.event;
-  const slug = req.body.data.id;
-  if(hookType === "post.published" || hookType === "post.update"){
-    await addPost(slug);
-  } else if (hookType === "post.delete"){
-    await archivePostData(slug);
+export const handleWebhook = functions.https.onRequest(async (req, res) => {
+  const event = req.body.webhook.event;
+  const eventSplit = event.split('.');
+  const action = eventSplit[2];
+  let type;
+  if(eventSplit[1] === 'post'){
+    type = 'blog';
+  }else{
+    type = req.body.data.page_type;
   }
-  comparePosts();
+  const slug = req.body.data.id;
+  if(action === "published" || action === "update"){
+    await addItem(slug, type);
+  } else if (action === "delete"){
+    await archiveItemData(slug, type);
+  }
+  compareDatabases();
   res.end();
 });
-
-export const handlePageWebhook = functions.https.onRequest(async (req, res) => {
-  const hookType = req.body.webhook.event;
-  const slug = req.body.data.id;
-  const pageType = req.body.data.page_type;
-  if(hookType === "page.update"){
-    addPage(slug, pageType);
-  } else if (hookType === "page.delete"){
-    archivePageData(slug, pageType);
-  }
-  compareCases();
-  res.end();
-});
-
-const addPost= async function(slug) {
-  await admin.database().ref(`/blog/blog-slugs/${slug}`).set(timeStamp);
-  const result = await getPostBySlug(slug);
-  await admin.database().ref(`/blog/blog-data/${slug}`).set(result.data);
-  return;
-}
-
-const archivePostData = async function(slug) {
-  const result = await admin.database().ref(`/blog/blog-data/${slug}`).once('value');
-  await admin.database().ref(`/blog/blog-data-archive/${slug}`).set(result.val());
-  await admin.database().ref(`/blog/blog-slugs/${slug}`).remove();
-  await admin.database().ref(`/blog/blog-data/${slug}`).remove();
-  return;
-}
 
 const getPostBySlug = async function(slug) {
   try {
@@ -56,72 +36,6 @@ const getPostBySlug = async function(slug) {
   } catch (error) {
     console.log(error);
   }
-}
-
-const getPostsCMS = async function() {
-  const posts = await butter.post.list();
-  return posts.data.data;
-}
-
-const getPostsFirebase = async function() {
-  const posts = await admin.database().ref(`blog/blog-data`).once('value');
-  return posts.val();
-}
-
-const comparePosts = async function(){
-  const postsCMS = await getPostsCMS();
-  const postsFirebase = await getPostsFirebase();
-  const postsFirebaseKeys = Object.keys(postsFirebase);
-  const postsCMSSlugs=[];
-  postsCMS.forEach((item) => {
-    const slug = item.slug;
-    postsCMSSlugs.push(slug);
-    const object = postsFirebase[slug];
-    if(!object){
-      addPost(slug);
-    }
-  });
-  postsFirebaseKeys.forEach((slug) => {
-    if(postsCMSSlugs.indexOf(slug) === -1){
-      archivePostData(slug);
-    }
-  });
-  return;
-}
-
-const addPage = async function(slug, type) {
-  await admin.database().ref(`/${type}/${type}-slugs/${slug}`).set(timeStamp);
-  const result = await getPageBySlug(slug);
-  await admin.database().ref(`/${type}/${type}-data/${slug}`).set(result.data);
-  return;
-}
-
-const archivePageData = async function(slug, type) {
-  const result = await admin.database().ref(`/${type}/${type}-data/${slug}`).once('value');
-  await admin.database().ref(`/${type}/${type}-data-archive/${slug}`).set(result.val());
-  await admin.database().ref(`/${type}/${type}-slugs/${slug}`).remove();
-  await admin.database().ref(`/${type}/${type}-data/${slug}`).remove();
-  return;
-}
-
-const compareCases = async function(){
-  const casesCMS = await getCasesCMS();
-  const casesFirebase = await getCasesFirebase();
-  const casesFirebaseKeys = Object.keys(casesFirebase);
-  const casesCMSSlugs=[];
-  casesCMS.forEach((item) => {
-    const slug = item.slug;
-    casesCMSSlugs.push(slug);
-    const object = casesFirebase[slug];
-    if(!object){
-      addPage(slug, 'client_case_study');
-    }
-  });
-  casesFirebaseKeys.forEach((slug) => {
-    if(casesCMSSlugs.indexOf(slug) === -1){
-      archivePageData(slug, 'client_case_study');
-    }
-  });
 }
 
 const getPageBySlug = async function(slug) {
@@ -133,12 +47,62 @@ const getPageBySlug = async function(slug) {
   }
 }
 
-const getCasesCMS = async function() {
-  const cases = await butter.page.list('client_case_study');
-  return cases.data.data;
+const addItem = async function(slug, type) {
+  await admin.database().ref(`/${type}/${type}-slugs/${slug}`).set(timeStamp);
+  let result;
+  if(type === 'blog'){
+    result = await getPostBySlug(slug);
+  }else{
+    result = await getPageBySlug(slug);
+  }
+  await admin.database().ref(`/${type}/${type}-data/${slug}`).set(result.data);
+  return;
 }
 
-const getCasesFirebase = async function() {
-  const cases = await admin.database().ref(`client_case_study/client_case_study-data`).once('value');
+const archiveItemData = async function(slug, type) {
+  const result = await admin.database().ref(`/${type}/${type}-data/${slug}`).once('value');
+  await admin.database().ref(`/${type}/${type}-data-archive/${slug}`).set(result.val());
+  await admin.database().ref(`/${type}/${type}-slugs/${slug}`).remove();
+  await admin.database().ref(`/${type}/${type}-data/${slug}`).remove();
+  return;
+}
+
+const compareDatabases = async function(type){
+  let itemsCMS =
+  if(type === 'blog'){
+    itemsCMS = await getPostsCMS();
+  }else{
+    itemsCMS = await getPagesCMS(type);
+  }
+  const itemsFirebase = await getItemsFirebase(type);
+  const itemsFirebaseKeys = Object.keys(itemsFirebase);
+  const itemsCMSSlugs=[];
+  itemsCMS.forEach((item) => {
+    const slug = item.slug;
+    itemsCMSSlugs.push(slug);
+    const object = itemsFirebase[slug];
+    if(!object){
+      addItem(slug, type);
+    }
+  });
+  itemsFirebaseKeys.forEach((slug) => {
+    if(itemsCMSSlugs.indexOf(slug) === -1){
+      archiveItemData(slug, type);
+    }
+  });
+}
+
+const getPostsCMS = async function() {
+  const posts = await butter.post.list();
+  return posts.data.data;
+}
+
+const getPagesCMS = async function(type) {
+  const pages = await butter.page.list(type);
+  return pages.data.data;
+}
+
+const getItemsFirebase = async function(type) {
+  const cases = await admin.database().ref(`${type}/${type}-data`).once('value');
   return cases.val();
 }
